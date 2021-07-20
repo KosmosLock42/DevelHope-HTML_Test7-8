09b0ea053e697820959ab36fd9a05c8468bda5a3
KosmosLock <cos.lo.iac@gmail.com>
2021-07-20 20:35:47 +0200

    HTML7-8 Complete

---
+++ database.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grazie per l'iscrizione</title>
+</head>
+<body>
+
+<h1>Grazie per esserti registrato.</h1>
+
+<h3>(Database Placeholder)</h3>
+    
+</body>
+</html>
+
+
+<!-- Un'ipotetico file dove andranno a finire i dati -->
